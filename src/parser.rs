f05dfcf0b7d09e29cbe5fed93e1eb17753192627
Eduardo Bonachela <dudubonachela@gmail.com>
2022-03-04 18:43:38 -0300

    add paser nodes

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,14 @@
+use crate::tokens::Token;
+
 enum Node {
+    Number(Token),
     BinaryOperation {
-        
+        left: Token,
+        operation: Token,
+        right: Token
+    },
+    UnaryOperation {
+        operation: Token,
+        rigth: Token
     }
 }
